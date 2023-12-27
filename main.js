aa60bfc104f9c42ec7bea1e9c4e848dffccfaf8e
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2023-12-27 20:49:22 +0100

    gives correct change on chrome fails test

--- main.js
+++ main.js
@@ -9,37 +9,42 @@ let coinVal = [
 ]; 
 
 
-//the obj needs to be iterated for a later function that used both key&value
-//A for loop iterates and allows for a function to be completed after
+// the obj needs to be iterated for a later function that used both key&value
+// A for loop iterates and allows for a function to be completed after
 function calcChange(input, price, coinVal){
-    let changes = [];
-    for (let i = 0; i < coinVal.length; i++){
-        let change=coinVal[i][1]
-        if (input >= price){
-            change = input % price;    
-        }
-        changes.push(change);
-    }
-     return changes;   
-    }
-function calcCoin(changes, coinVal) {
-    let coinsNeeded= [];
-    for (let i = 0; i < changes.length; i++) {
-        let coinCount = Math.floor(changes[i] / coinVal[i][1]);
-        coinsNeeded.push([coinVal[i][0], coinCount]);
-    }
-   return coinsNeeded;
+    let changes = (input * 100) - (price * 100);
+    let changeDetails = [];
+    coinVal.sort((a, b) => b[1] - a[1]);
+    for (let i = 0; i < coinVal.length; i++) {
+        let coinType = coinVal[i][0];
+        let coinValue = coinVal[i][1] * 100;
+     if (changes >= coinValue) {
+        let coinCount = Math.floor(changes / coinValue);
+        changes -= coinCount * coinValue;
+        changeDetails.push([coinType, coinCount]);
+     }else{ 
+        changeDetails.push([coinType, 0]);
+     }  
 }  
-function handleClickEvent(){
+    return changeDetails;
+}
+// function handleClickEvent(){
+//     let input = parseFloat(document.getElementById('amount-received').value);
+//     let price = parseFloat(document.getElementById('amount-due').value);
+//     let changes = calcChange(input, price, coinVal);
+//     let coinsNeeded = calcCoin(changes, coinVal)
+function handleClickEvent() {
     let input = parseFloat(document.getElementById('amount-received').value);
     let price = parseFloat(document.getElementById('amount-due').value);
-    let changes = calcChange(input, price, coinVal);
-    let coinsNeeded = calcCoin(changes, coinVal)
+    let changeDetails = calcChange(input, price, coinVal);
+    console.log(changeDetails);
+
 // document.querySelector('#output').innerHTML = JSON.stringify(coinsNeeded);
-document.getElementById('dollars-value').innerHTML = coinsNeeded[4][1]; // Assuming 'dollars' is the last element in coinVal
-    document.getElementById('quarters-value').innerHTML = coinsNeeded[3][1];
-    document.getElementById('dimes-value').innerHTML = coinsNeeded[2][1];
-    document.getElementById('nickels-value').innerHTML = coinsNeeded[1][1];
-    document.getElementById('pennies-value').innerHTML = coinsNeeded[0][1];
+    document.getElementById('dollars-value').innerHTML = changeDetails[0][1]; 
+    document.getElementById('quarters-value').innerHTML = changeDetails[1][1];
+    document.getElementById('dimes-value').innerHTML = changeDetails[2][1];
+    document.getElementById('nickels-value').innerHTML = changeDetails[3][1];
+    document.getElementById('pennies-value').innerHTML = changeDetails[4][1];
 }
+
 document.querySelector('#calculate-change').addEventListener('click', handleClickEvent);
