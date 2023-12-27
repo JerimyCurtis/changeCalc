ea6923ded8e5d837e91e652db819e2c115f0204d
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2023-12-27 02:04:21 +0100

    tried to implement the innerHTML

--- main.js
+++ main.js
@@ -8,25 +8,38 @@ let coinVal = [
     ['dollars', 100]
 ]; 
 
-var input = parseFloat(document.getElementById('Amount'));
-var price = parseFloat(dicument.getElementById)('Price');
+
 //the obj needs to be iterated for a later function that used both key&value
 //A for loop iterates and allows for a function to be completed after
 function calcChange(input, price, coinVal){
+    let changes = [];
     for (let i = 0; i < coinVal.length; i++){
         let change=coinVal[i][1]
         if (input >= price){
             change = input % price;    
         }
         changes.push(change);
-
+    }
+     return changes;   
+    }
 function calcCoin(changes, coinVal){
     let coinsNeeded= [];
-            for (let i = 0; i < changes.length; i++) {
-                let coinCount = Math.floor(changes[i] / coinVal[i][1]);
-                coinsNeeded.push([coinVal[i][0]], coinCount);
-            }
-            return coinsNeeded;
-    }
+    for (let i = 0; i < changes.length; i++) {
+        let coinCount = Math.floor(changes[i] / coinVal[i][1]);
+        coinsNeeded.push([coinVal[i][0]], coinCount);
+   }
+   return coinsNeeded;
+}  
+function handleClickEvent(){
+    let input = parseFloat(document.getElementById('amount-received').value);
+    let price = parseFloat(document.getElementById('amount-due').value);
+    let changes = calcChange(input, price, coinVal);
+    let coinsNeeded = calcCoin(changes, coinVal)
+// document.querySelector('#output').innerHTML = JSON.stringify(coinsNeeded);
+document.getElementById('dollars-value').innerHTML = coinsNeeded[4][1]; // Assuming 'dollars' is the last element in coinVal
+    document.getElementById('quarters-value').innerHTML = coinsNeeded[3][1];
+    document.getElementById('dimes-value').innerHTML = coinsNeeded[2][1];
+    document.getElementById('nickels-value').innerHTML = coinsNeeded[1][1];
+    document.getElementById('pennies-value').innerHTML = coinsNeeded[0][1];
 }
-}
+document.querySelector('#calculate-change').addEventListener('click', handleClickEvent);
