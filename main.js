6129c08a12088f117f5f628819feec05027320d7
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2023-12-28 23:14:46 +0100

    Final

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//the goal is to create a calculator that outputs the correct change
-//the change needs to be defined in a way that can be iterated
 let coinVal = [
     ['pennies', .01], 
     ['nickles', .05], 
@@ -12,9 +10,6 @@ let coinVal = [
     ['$20', 20.0]
 ]; 
 
-
-// the obj needs to be iterated for a later function that used both key&value
-// A for loop iterates and allows for a function to be completed after
 function calcChange(input, price, coinVal){
     let changes = (input * 100) - (price * 100);
     let changeDetails = [];
@@ -40,7 +35,6 @@ function calcTotal (changeDetails) {
     return total.toFixed(2);
 }
 
-
 function handleClickEvent() {
     let input = parseFloat(document.getElementById('amount-received').value);
     let price = parseFloat(document.getElementById('amount-due').value);
@@ -48,7 +42,6 @@ function handleClickEvent() {
     let totalChange = calcTotal(changeDetails);
     console.log(changeDetails);
 
-// document.querySelector('#output').innerHTML = JSON.stringify(coinsNeeded);
     document.getElementById('20-dollars-value').innerHTML = changeDetails[0][1]
     document.getElementById('10-dollars-value').innerHTML = changeDetails[1][1]
     document.getElementById('5-dollars-value').innerHTML = changeDetails[2][1]
